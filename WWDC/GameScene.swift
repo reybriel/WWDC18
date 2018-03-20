61079aa9ff4f4184f9ed75343c744a03ea8ac2cc
Gabriel Reynoso <folameloma@gmail.com>
2018-03-20 14:21:22 -0300

    Info.plist setups

--- WWDC/GameScene.swift
+++ WWDC/GameScene.swift
@@ -84,6 +84,6 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        print("update - \(currentTime)")
     }
 }
